fd9219fea9f0154f7257a11efe5d0ed4d515c12c
practice20200 <wor9hard2018@gmail.com>
2022-06-19 13:07:52 -0700

    initial commit

---
+++ SWIFTUIQRCodeGeneratorPractice/SWIFTUIQRCodeGeneratorPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFTUIQRCodeGeneratorPracticeApp.swift
+//  SWIFTUIQRCodeGeneratorPractice
+//
+//  Created by Apple New on 2022-06-19.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTUIQRCodeGeneratorPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
