e5d4c2f3da5cce401351efd0d9fb1cc127af7f02
practice20200 <wor9hard2018@gmail.com>
2022-06-19 14:09:11 -0700

    Created function to create QRCode

---
+++ SWIFTUIQRCodeGeneratorPractice/QRCodeGenerator.swift
@@ -0,0 +1,36 @@
+//
+//  QRCodeGenerator.swift
+//  SWIFTUIQRCodeGeneratorPractice
+//
+//  Created by Apple New on 2022-06-19.
+//
+
+import Foundation
+import UIKit
+import CoreImage.CIFilterBuiltins
+
+struct QRCodeGenerator {
+    private let context = CIContext()
+    private let filter = CIFilter.qrCodeGenerator()
+    
+    public func generateQRCode(forURLString urlString: String) -> QRCode? {
+        guard !urlString.isEmpty else { return nil}
+        
+        let data = Data(urlString.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+        
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent){
+                let qrCode = QRCode(urlString: urlString, uiImage: UIImage(cgImage: cgImage))
+                return qrCode
+            }
+        }
+        return nil
+    }
+}
+
+
+struct QRCode {
+    let urlString: String
+    let uiImage: UIImage
+}
